01ad7cb5f54f13fb05f148d0a619aec7b8f43822
Nick Rock <NickRock625@yahoo.com>
2023-07-03 11:31:33 -0400

    Add project files.

---
+++ Tile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1
+{
+    internal class Tile
+    {
+        public string face;
+        
+
+        public Tile()
+        {
+            face = "##";
+            
+        }
+
+        public string getFace() { return face; }
+        public void setFace(string face) { this.face = face; }
+
+    }
+}
---
+++ maze.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1
+{
+    internal class maze
+    {
+        /*
+            ## 00 ## ## ## ## ## ## ## ## ## ##
+         *  ## 01 02 [] [] ## [] [] [] [] [] ##
+         *  ## 03 ## ## [] ## [] ## ## ## [] ##
+         *  ## [] ## [] [] ## [] [] [] ## [] []
+         *  ## [] ## ## ## ## ## ## [] ## ## ##
+         *  ## [] [] ## [] [] [] ## [] [] [] ##
+         *  ## ## [] ## [] ## ## ## [] ## [] ##
+         *  ## [] [] [] [] ## [] [] [] ## [] ##
+         *  ## [] ## ## ## ## [] ## [] ## ## ##
+         *  ## [] [] [] [] [] [] ## [] [] [] ##
+         *  ## ## ## ## ## ## ## ## ## ## ## ##
+         */
+
+        Dictionary<Tuple<byte, byte>, Tile> tileSet = new Dictionary<Tuple<byte, byte>, Tile>();
+
+        public maze() 
+        {
+            for(byte i = 0; i < 12; i++)
+            {
+                for(byte j = 0; j < 11; j++)
+                {
+                    tileSet.Add(new Tuple<byte, byte>(j,i), new Tile());
+                }
+            }
+        }
+
+        public void makePath(List<Tuple<byte, byte>> pathSpaces) //this is way easier than c++ LOL
+        {
+            foreach(Tuple<byte, byte> t in pathSpaces)
+            {
+                if(tileSet.ContainsKey(t))
+                {
+                    tileSet[t].setFace("[]");
+                }
+            }
+        }
+
+        public void printMaze()
+        {
+
+        }
+
+
+
+        //12 wide 11 "tall"
+        //X increases rightward, Y increases downward
+        //132 items!
+    }
+}
